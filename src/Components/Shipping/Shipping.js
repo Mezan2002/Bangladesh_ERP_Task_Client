550ba80c148dc4e4e36ec108cd6ae673f8f5542a
Mezan2002 <mezanurr009@gmail.com>
2023-01-03 01:38:51 +0600

    Shipping field created

--- src/Components/Shipping/Shipping.js
+++ src/Components/Shipping/Shipping.js
@@ -112,20 +112,134 @@ const Shipping = () => {
               </select>
             </div>
             <div>
-              <label>Attention</label>
-              <input type="text" name="" placeholder="Enter Person/Site Name" />
+              <label>House</label>
+              <input type="text" name="" />
             </div>
             <div>
-              <label>Attention</label>
-              <input type="text" name="" placeholder="Enter Person/Site Name" />
+              <label>Phone</label>
+              <input type="text" name="" />
             </div>
             <div>
-              <label>Attention</label>
-              <input type="text" name="" placeholder="Enter Person/Site Name" />
+              <label>Fax</label>
+              <input type="text" name="" />
             </div>
           </div>
           <div className="shippingAdress">
-            <h3>shipping Address</h3>
+            <h3>shipping Address </h3>
+            <div>
+              <label>Attention</label>
+              <input type="text" name="" placeholder="Enter Person/Site Name" />
+            </div>
+            <div>
+              <label>Country</label>
+              <select name="">
+                <option selected disabled>
+                  Please Search
+                </option>
+                <option value="">Bangladesh</option>
+                <option value="">Brazil</option>
+                <option value="">Argentina</option>
+                <option value="">India</option>
+                <option value="">Nepal</option>
+                <option value="">Pakistan</option>
+              </select>
+            </div>
+            <div>
+              <label>Country</label>
+              <select name="">
+                <option selected disabled>
+                  Please Search
+                </option>
+                <option value="">Bangladesh</option>
+                <option value="">Brazil</option>
+                <option value="">Argentina</option>
+                <option value="">India</option>
+                <option value="">Nepal</option>
+                <option value="">Pakistan</option>
+              </select>
+            </div>
+            <div>
+              <label>Country</label>
+              <select name="">
+                <option selected disabled>
+                  Please Search
+                </option>
+                <option value="">Bangladesh</option>
+                <option value="">Brazil</option>
+                <option value="">Argentina</option>
+                <option value="">India</option>
+                <option value="">Nepal</option>
+                <option value="">Pakistan</option>
+              </select>
+            </div>
+            <div>
+              <label>Country</label>
+              <select name="">
+                <option selected disabled>
+                  Please Search
+                </option>
+                <option value="">Bangladesh</option>
+                <option value="">Brazil</option>
+                <option value="">Argentina</option>
+                <option value="">India</option>
+                <option value="">Nepal</option>
+                <option value="">Pakistan</option>
+              </select>
+            </div>
+            <div>
+              <label>Country</label>
+              <select name="">
+                <option selected disabled>
+                  Please Search
+                </option>
+                <option value="">Bangladesh</option>
+                <option value="">Brazil</option>
+                <option value="">Argentina</option>
+                <option value="">India</option>
+                <option value="">Nepal</option>
+                <option value="">Pakistan</option>
+              </select>
+            </div>
+            <div>
+              <label>Country</label>
+              <select name="">
+                <option selected disabled>
+                  Please Search
+                </option>
+                <option value="">Bangladesh</option>
+                <option value="">Brazil</option>
+                <option value="">Argentina</option>
+                <option value="">India</option>
+                <option value="">Nepal</option>
+                <option value="">Pakistan</option>
+              </select>
+            </div>
+            <div>
+              <label>Country</label>
+              <select name="">
+                <option selected disabled>
+                  Please Search
+                </option>
+                <option value="">Bangladesh</option>
+                <option value="">Brazil</option>
+                <option value="">Argentina</option>
+                <option value="">India</option>
+                <option value="">Nepal</option>
+                <option value="">Pakistan</option>
+              </select>
+            </div>
+            <div>
+              <label>House</label>
+              <input type="text" name="" />
+            </div>
+            <div>
+              <label>Phone</label>
+              <input type="text" name="" />
+            </div>
+            <div>
+              <label>Fax</label>
+              <input type="text" name="" />
+            </div>
           </div>
         </div>
       </div>
